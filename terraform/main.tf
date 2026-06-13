e522f3918e4d7b977669c8cd9fcdd70ca0c04b41
Akshay Sunil <akshaymsunil@gmail.com>
2026-06-13 22:23:58 +0530

    Add terraform-managed Lambda and IAM resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,61 @@ resource "aws_dynamodb_table" "visitor_counter" {
     type = "S"
   }
 }
+resource "aws_iam_role" "lambda_role" {
+  name = "visitor-counter-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+resource "aws_lambda_function" "visitor_counter" {
+
+  function_name = "VisitorCounterTerraform"
+
+  role = aws_iam_role.lambda_role.arn
+
+  runtime = "python3.13"
+
+  handler = "lambda_function.lambda_handler"
+
+  filename = "../lambda/visitor-counter.zip"
+
+  source_code_hash = filebase64sha256("../lambda/visitor-counter.zip")
+}
+resource "aws_iam_role_policy" "dynamodb_access" {
+
+  name = "visitor-counter-dynamodb-policy"
+
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ]
+
+        Resource = aws_dynamodb_table.visitor_counter.arn
+      }
+    ]
+  })
+}
